30a2926aee4ec49e2118063228e65d7dd5ba9b76
Alex Stophel <alexstophel@gmail.com>
2016-08-02 19:40:36 -0500

    Initial commit.

---
+++ BoutTime/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BoutTime
+//
+//  Created by Alex Stophel on 8/2/16.
+//  Copyright © 2016 Alex Stophel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
